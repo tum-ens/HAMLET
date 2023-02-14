e3c5f9a74fff7fdb133ca5357869ed8cd2300db8
Markus Doepfert <markus.doepfert@tum.de>
2023-02-14 11:43:58 +0100

    Add lama analyzer

---
+++ lama/analyzer/setup.py
@@ -0,0 +1,7 @@
+__author__ = "TUM-Doepfert"
+__credits__ = ""
+__license__ = ""
+__maintainer__ = "TUM-Doepfert"
+__email__ = "markus.doepfert@tum.de"
+
+# TODO: Sets up the analysis part and makes sure that all figures are consistent in style
