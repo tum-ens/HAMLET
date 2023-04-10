87c326a7ac098e7bc70cf598e206149b885e0216
Jiahe Chu <jiahe.chu@tum.de>
2023-04-10 18:09:28 +0200

    deleted all agents.xlsx

--- 01 - examples/01_create_scenario.py
+++ 01 - examples/01_create_scenario.py
@@ -2,10 +2,10 @@ from hamlet import Scenario
 from time import time
 
 start = time()
-path = "../02 - config/example_small"     # relative or absolute path to the config folder
+path = "../02 - config/example_big"     # relative or absolute path to the config folder
 
 scenario = Scenario(path=path)
-# scenario.new_scenario_from_configs()
+scenario.new_scenario_from_configs()
 # scenario.new_scenario_from_grids()
-scenario.new_scenario_from_files()
+# scenario.new_scenario_from_files()
 print(f'Elapsed time: {time() - start:.2f} seconds')
