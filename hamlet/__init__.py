f260f267b29cf334b6d08bae15da38f72a625770
Markus Doepfert <markus.doepfert@tum.de>
2023-04-11 14:37:14 +0200

    Addition of Executor in init
    To call the functionality

--- hamlet/__init__.py
+++ hamlet/__init__.py
@@ -1,3 +1,3 @@
 from hamlet.creator.setup import Scenario
-# TODO: from lama.executor.setup import Executor
-# TODO: from lama.analyzer.setup import Analyzer
+from hamlet.executor.setup import Executor
+# TODO: from hamlet.analyzer.setup import Analyzer
