d65baa08cd2957a4a945631d54c70af9a9dfec45
Markus Doepfert <markus.doepfert@tum.de>
2023-04-11 15:52:28 +0200

    Initial files for agents, grids and markets
    So far just contain thoughts and general stuff

---
+++ hamlet/executor/markets/markets.py
@@ -0,0 +1,21 @@
+__author__ = "MarkusDoepfert"
+__credits__ = ""
+__license__ = ""
+__maintainer__ = "MarkusDoepfert"
+__email__ = "markus.doepfert@tum.de"
+
+# This file is in charge of handling the markets in the execution of the scenario
+
+# Imports
+import os
+import pandas as pd
+import polars as pl
+import numpy as np
+import time
+import logging
+import traceback
+from datetime import datetime
+
+# TODO: Considerations
+# - Use polars instead of pandas to increase performance
+# - Use parallel computing for market clearings
