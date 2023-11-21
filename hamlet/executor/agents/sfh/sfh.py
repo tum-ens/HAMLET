422143e2f301fdf596a0a500c464c2b58a88ca61
Markus Doepfert <markus.doepfert@tum.de>
2023-11-21 11:58:23 +0100

    Synchronize agent structure

--- hamlet/executor/agents/sfh/sfh.py
+++ hamlet/executor/agents/sfh/sfh.py
@@ -6,7 +6,7 @@ __email__ = "markus.doepfert@tum.de"
 
 # Imports
 import hamlet.constants as c
-from hamlet.executor.agents.agents_base import AgentBase
+from hamlet.executor.agents.agent_base import AgentBase
 
 
 class Sfh(AgentBase):
