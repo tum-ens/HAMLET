f5593ec2bc0a5c8bcce8cd1214ee10a4b7ba42f9
Markus Doepfert <markus.doepfert@tum.de>
2023-11-21 11:59:39 +0100

    Updated description

--- hamlet/executor/utilities/controller/controller_base.py
+++ hamlet/executor/utilities/controller/controller_base.py
@@ -4,8 +4,7 @@ __license__ = ""
 __maintainer__ = "MarkusDoepfert"
 __email__ = "markus.doepfert@tum.de"
 
-# Note: Used to contain the component mapping which was moved to constants.py to give universal access.
-# For now no specific use case for this class but kept for consistency and possible future uses.
+# Used to ensure a consistent design of all markets
 
 
 class ControllerBase:
