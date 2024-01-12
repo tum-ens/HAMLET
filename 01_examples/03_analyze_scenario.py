760055059849971b2d9876a923c3453bd91f7398
Markus Doepfert <markus.doepfert@tum.de>
2024-01-12 10:29:02 +0100

    Enable VSCode runability
    Otherwise it does not recognize the hamlet folder as package.

--- 01_examples/03_analyze_scenario.py
+++ 01_examples/03_analyze_scenario.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("..")  # Add the parent directory to the Python path for execution outside of an IDE
+sys.path.append("./")  # Add the current directory to the Python path for execution in VSCode
 from hamlet import Analyzer
 
 # Path to the results folder (relative or absolute)
