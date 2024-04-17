cdf0dec0683ecc1bd3cf203e74a5c4f297cd7745
Markus Döpfert <77633211+TUM-Doepfert@users.noreply.github.com>
2024-04-17 10:29:44 +0200

    Update 02_execute_scenario.py

--- 01_examples/02_execute_scenario.py
+++ 01_examples/02_execute_scenario.py
@@ -3,40 +3,11 @@ sys.path.append("..")  # Add the parent directory to the Python path for executi
 sys.path.append("./")  # Add the current directory to the Python path for execution in VSCode
 from hamlet import Executor
 
-
-list_scenarios = ["individual", "prosumer", "consumer", "mixed"]
-list_seasons = ["summer", "transition", "winter"]
-list_group_size = ["5", "10", "15"]
-list_forecasting_methods = ["naive", "perfect"]
-
 # Path to the scenario folder (relative or absolute)
+path = "../04_scenarios/example_single_market"
 
+# Create the executor object
+sim = Executor(path, num_workers=1)
 
-for scenario in list_scenarios:
-    if scenario == "individual":
-        for season in list_seasons:
-            for forecasting_method in list_forecasting_methods:
-                path = "../04_scenarios/scenario_pv_60_hp_60_ev_60_" + scenario + "_" + season + "_" + forecasting_method
-
-                # Create the executor object
-                sim = Executor(path, num_workers=1)
-
-                # Run the simulation
-                sim.run()
-
-    else:
-        for season in list_seasons:
-            for group in list_group_size:
-                for forecasting_method in list_forecasting_methods:
-
-                    path = "../04_scenarios/scenario_pv_60_hp_60_ev_60_" + scenario + "_" + group + "_" + season + "_" + forecasting_method
-
-                    # Create the executor object
-                    sim = Executor(path, num_workers=1)
-
-                    # Run the simulation
-                    sim.run()
-
-
-
-
+# Run the simulation
+sim.run()
