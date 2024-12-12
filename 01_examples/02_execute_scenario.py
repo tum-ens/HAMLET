60fe73706ac403e8670dfacabefe081a1f88d516
Lukas <oss.heindl+git@protonmail.com>
2024-12-12 14:53:41 +0100

    todo revert: make amount of workers variable

--- 01_examples/02_execute_scenario.py
+++ 01_examples/02_execute_scenario.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.append("..")  # Add the parent directory to the Python path for execution outside an IDEimport sys
 sys.path.append("./")  # Add the current directory to the Python path for execution in VSCode
 from hamlet import Executor
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     path = "../04_scenarios/example_single_market"
 
     # Create the executor object
-    sim = Executor(path, num_workers=2)
+    sim = Executor(path, num_workers=int(os.environ["NUM_WORKERS"]))
 
     # Run the simulation
     sim.run()
