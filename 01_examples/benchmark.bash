0fb244c8e4e565bbe04ea6ce6ec8f6258d8cbb0b
Lukas <oss.heindl+git@protonmail.com>
2024-12-16 09:10:44 +0100

    save benchmark results not in local form

--- 01_examples/benchmark.bash
+++ 01_examples/benchmark.bash
@@ -4,7 +4,7 @@ set -e
 
 for num_workers in 1 2 4 ; do
 	make clean_local
-	NUM_WORKERS="${num_workers}" make profile_perf_local
-	NUM_WORKERS="${num_workers}" make profile_memory_local
+	NUM_WORKERS="${num_workers}" make profile_perf
+	NUM_WORKERS="${num_workers}" make profile_memory
 	make SAVE_BASE="worker${num_workers}_" save_all
 done
