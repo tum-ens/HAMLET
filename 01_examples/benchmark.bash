4186faa8a294360956e36c3990ead1569be59d79
Lukas <oss.heindl+git@protonmail.com>
2024-12-16 09:08:23 +0100

    adjust benchmark params

--- 01_examples/benchmark.bash
+++ 01_examples/benchmark.bash
@@ -2,7 +2,7 @@
 
 set -e
 
-for num_workers in 1 2 4 8 ; do
+for num_workers in 1 2 4 ; do
 	make clean_local
 	NUM_WORKERS="${num_workers}" make profile_perf_local
 	NUM_WORKERS="${num_workers}" make profile_memory_local
