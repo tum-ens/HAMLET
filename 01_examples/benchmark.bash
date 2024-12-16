9c0f2b20df9367cfc97a2876f362778cd0b7f4ee
Lukas <oss.heindl+git@protonmail.com>
2024-12-16 14:25:58 +0100

    fixup benchmark

--- 01_examples/benchmark.bash
+++ 01_examples/benchmark.bash
@@ -3,9 +3,10 @@
 set -e
 
 for num_workers in 1 2 4 ; do
-	git restore ../02_config/example_single_market/agents.xlsx
 	make clean_local
+	git restore ../02_config/example_single_market/agents.xlsx
 	NUM_WORKERS="${num_workers}" make profile_perf
+	git restore ../02_config/example_single_market/agents.xlsx
 	NUM_WORKERS="${num_workers}" make profile_memory
 	make SAVE_BASE="worker${num_workers}_" save_all
 done
