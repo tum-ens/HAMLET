148774df3ec2a66c8094b53017ae9b01641f13b5
Lukas <oss.heindl+git@protonmail.com>
2024-12-16 08:39:07 +0100

    todo revert/remove: add makefile and script for benchmarking

---
+++ 01_examples/benchmark.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+for num_workers in 1 2 4 8 ; do
+	make clean_local
+	NUM_WORKERS="${num_workers}" make profile_perf_local
+	NUM_WORKERS="${num_workers}" make profile_memory_local
+	make SAVE_BASE="worker${num_workers}_" save_all
+done
