3ff63c59714182fa3f95990c1a6452bda6068464
TUM-Doepfert <markus.doepfert@tum.de>
2025-03-25 18:51:38 +0100

    Create minimal run file

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
 import sys
 sys.path.append("..")  # Add the parent directory to the Python path for execution outside an IDEimport sys
 sys.path.append("./")  # Add the current directory to the Python path for execution in VSCode
-from hamlet import Executor
+from hamlet import Creator, Executor, Analyzer
 
-# Path to the scenario folder (relative or absolute)
-path = "./04_scenarios/example_single_market"
 
-# Create the executor object
-sim = Executor(path, num_workers=1)
+def create_scenario(path: str, name: str = None):
+    Creator(path, name=name).new_scenario_from_configs()
+    # Creator(path).new_scenario_from_grids(name=name)
+    # Creator(path).new_scenario_from_files(name=name)
 
-# Run the simulation
-sim.run()
+
+def run_scenario(path: str):
+    Executor(path, num_workers=1).run()
+
+
+def analyze_scenario(path: str):
+    Analyzer(path).plot_general_analysis()
+
+
+if __name__ == '__main__':
+    # This is a minimal non-working example on how you would use the framework.
+    # For working examples, please refer to the "examples" folder.
+    path = 'path to config folder'
+    create_scenario(path)
+    path = 'path to scenario folder'
+    run_scenario(path)
+    path = 'path to results folder'
+    analyze_scenario(path)
+
