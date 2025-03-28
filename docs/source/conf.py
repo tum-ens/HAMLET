9db678330088242533f89ad677daeb9fdfe75a94
TUM-Doepfert <markus.doepfert@tum.de>
2025-03-28 11:18:52 +0100

    Consolidate conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,8 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../hamlet'))  # Adjust if needed
 
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Adjust the path as needed
+sys.path.insert(0, os.path.abspath('../../hamlet'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -17,8 +14,6 @@ release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-sys.path.insert(0, os.path.abspath('../../hamlet'))  # Adjust the path as needed
-
 
 extensions = [
     'sphinx.ext.autodoc',    # Extracts docstrings for API docs
@@ -26,7 +21,6 @@ extensions = [
     'sphinx.ext.viewcode',   # Adds source code links
     'sphinx.ext.autosummary' # Summarizes modules and classes
 ]
-
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -37,32 +31,5 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../hamlet'))
-extensions.append('sphinx.ext.autosummary')
-html_theme = 'sphinx_rtd_theme'
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../hamlet'))  # Adjust as needed
-
-# Sphinx extensions
-extensions += ['sphinx.ext.autosummary', 'sphinx.ext.napoleon']
-
-# HTML theme
-html_theme = 'sphinx_rtd_theme'
-    
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../hamlet'))  # Adjust as needed
-
-# Sphinx extensions
-extensions += ['sphinx.ext.autosummary', 'sphinx.ext.napoleon']
-
-# HTML theme
-html_theme = 'sphinx_rtd_theme'
-
 # Generate autosummary files
 autosummary_generate = True
-    
